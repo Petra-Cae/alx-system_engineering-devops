0e133722b8d03e67f3c1a4c0fffe8a74241d3773
Petra <111303824+Petra-Cae@users.noreply.github.com>
2023-08-10 16:20:44 +0100

    Indentation fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes a WordPress issue where Apache was returning a 500 error
 
 exec { 'wordpress-fix':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path     => '/usr/local/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/'
 }
